cee6bfdde7640b5e80c04940637e760a40482ee0
Carlos Soto <ivoodoo@carloss-mac.local>
2015-11-07 02:12:31 -0700

    mensaje del commit chala

--- ProbandoGit/ViewController.swift
+++ ProbandoGit/ViewController.swift
@@ -5,11 +5,12 @@
 //  Created by Carlos Soto on 11/7/15.
 //  Copyright © 2015 Carlos Soto. All rights reserved.
 //
+//lalalalalala
 
 import UIKit
 
 class ViewController: UIViewController {
-
+//editando y probando xxxhh
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
